4c37fd0c20cddd3b489c7a33db472a151f073993
Diogo Araújo <61635272+DMdSA@users.noreply.github.com>
2020-03-14 16:57:46 +0000

    Update logicaPrograma.h

--- logicaPrograma.h
+++ logicaPrograma.h
@@ -1,6 +1,9 @@
 #ifndef LOGICA_FILE
 #define LOGICA_FILE
 
-int jogar(ESTADO *e, COORDENADA c);
+int verifica_Posicao_Jogada (ESTADO *e, COORDENADA c);
+int jogar (ESTADO *e, COORDENADA c);
+int verifica_CASA (ESTADO *e, COORDENADA c);
+int verifica_PERDEU (ESTADO *e, COORDENADA c);
 
 #endif
