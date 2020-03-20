894ed4f85626cd9644fcc23f33838f36d3a5bf0f
Diogo Miguel Silva Araújo <61635272+DMdSA@users.noreply.github.com>
2020-03-20 13:20:20 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "bases.h"
 #include "camadaDados.h"
 #include "logicaPrograma.h"
 #include "interfacePrograma.h"
 
-
 int main() {
 
 	ESTADO *e = inicializar_estado();
-
+	mostrar_tabuleiro(e);
+	printf("\n");
+	
 	interpretador(e);
 	
 	return 0;
