7cdde77200603c7e808c438c694cc9ea750d6255
Diogo Miguel Silva Araújo <61635272+DMdSA@users.noreply.github.com>
2020-03-20 13:19:48 +0000

    Update interfacePrograma.h

--- interfacePrograma.h
+++ interfacePrograma.h
@@ -1,13 +1,13 @@
 #ifndef INTERFACE_FILE
 #define INTERFACE_FILE
 
-void prompt_INFO (ESTADO *e)
+void prompt_INFO (ESTADO *e);
 
 void mostrar_tabuleiro(ESTADO *e);
 
-void gr (ESTADO *e);
+void gr (FILE *jogadaAtual, ESTADO *e);
 
-void ler (ESTADO *e);
+void ler (FILE *ficheiro);
 
 int interpretador(ESTADO *e);
 
