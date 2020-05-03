24baa048e511b6cc6c15c7382402741eedbe648c
Diogo Miguel Silva Araújo <61635272+DMdSA@users.noreply.github.com>
2020-05-03 15:58:40 +0100

    Delete compile.bat

--- compile.bat
+++
@@ -1,7 +0,0 @@
-gcc -c logicaPrograma.c
-gcc -c interfacePrograma.c
-gcc -c camadaDados.c
-gcc -c lista.c
-gcc -c main.c
-
-gcc -o diogo logicaPrograma.o interfacePrograma.o camadaDados.o main.o lista.o
